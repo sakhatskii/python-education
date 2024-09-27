4f47676450e976c8c36c65d1bf3eebd8bf63675c
Aleksandr S.Sakhatskii <sakhackii@protei-lab.ru>
2024-09-27 19:15:56 +0300

    added decorator

---
+++ python_education/decorator_and_closure/decorator.py
@@ -0,0 +1,18 @@
+def debug_decorator(function):  # "обертывеаем" функцию add новыми свойствами
+    def wrapper(*args):
+        print(f"Вызываем функцию {function.__name__}")
+        result = function(*args)
+        print(f"Результат функции {function.__name__}: {result}")
+        print(f"Результат перемножения аргументов функции {function.__name__}: {args[0] * args[1]}")
+        return result
+    return wrapper
+
+@debug_decorator
+def add(x, y):
+  return x + y
+
+add(90, 40)
+
+# Вызываем функцию add
+# Результат функции add: 130
+# Результат перемножения аргументов функции add: 3600
