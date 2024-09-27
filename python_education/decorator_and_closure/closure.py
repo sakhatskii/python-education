3362afcd9120dfa5bfec53103e774cf8b5b37463
Aleksandr S.Sakhatskii <sakhackii@protei-lab.ru>
2024-09-27 18:53:13 +0300

    fix

--- python_education/decorator_and_closure/closure.py
+++ python_education/decorator_and_closure/closure.py
@@ -1,8 +1,9 @@
-def outer_function(x):  # внешняя функция, создающая замыкание
-    def inner_function(y):  # внутренняя функция, использующая значение x
-        return x * y
-    return inner_function
+class Closure:
+    def outer_function(self, x):  # внешняя функция, создающая замыкание
+        def inner_function(y):  # внутренняя функция, использующая значение x
+            return x * y
+        return inner_function
 
-closure = outer_function(7)  # cоздаем замыкание с x == 7
+closure = Closure().outer_function(7)  # cоздаем замыкание с x == 7
 
 print(closure(5))  # 35 (7 * 5)
