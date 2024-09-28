33fda80f8263b7d9375d70c9a55b742295988c13
Aleksandr S.Sakhatskii <sakhackii@protei-lab.ru>
2024-09-28 14:29:15 +0300

    added dataclass

---
+++ python_education/dataclass/dataclass.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+class Cars:
+    def __init__(self, brand, model, vin, horsepower):
+        self.brand = brand
+        self.model = model
+        self.vin = vin
+        self.horsepower = horsepower
+
+ford = Cars("Ford", "Focus", "Z6F5XXEEC5FY02163", 125)
+print(f"Модель машины: {ford.brand}, мощность: {ford.horsepower} л.с.")
+
+
+@dataclass
+class Cars:
+    brand: str
+    model: str
+    vin: str
+    horsepower: int
+
+bmw = Cars(brand="BMW", model="X3", vin="VF3WCKFUC33681468", horsepower=260)
+print(f"Модель машины: {bmw.brand}, мощность: {bmw.horsepower} л.с.")
