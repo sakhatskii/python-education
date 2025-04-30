30a833be300488927154ea597b926409cbb9238f
Aleksandr S.Sakhatskii <sakhackii@protei-lab.ru>
2025-04-30 10:53:00 +0300

    fix linter

--- python_education/client_server/socket_client.py
+++ python_education/client_server/socket_client.py
@@ -4,7 +4,7 @@ host = "127.0.0.1"
 port = 12345
 
 client = socket.socket()
-client.connect((host, port))  #  подключение к серверу
+client.connect((host, port))  # подключение к серверу
 data = client.recv(1024)  # получаем данные с сервера, размер буфера 1024
 print("Server sent: ", data.decode())  # выводим данные на консоль
 client.close()
