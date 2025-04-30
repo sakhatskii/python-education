a34b671dca332865acad3a054e360ee5d0f0d3ff
Aleksandr S.Sakhatskii <sakhackii@protei-lab.ru>
2025-04-30 10:50:16 +0300

    add tcp server

---
+++ python_education/client_server/socket_client.py
@@ -0,0 +1,10 @@
+import socket
+
+host = "127.0.0.1"
+port = 12345
+
+client = socket.socket()
+client.connect((host, port))  #  подключение к серверу
+data = client.recv(1024)  # получаем данные с сервера, размер буфера 1024
+print("Server sent: ", data.decode())  # выводим данные на консоль
+client.close()
---
+++ python_education/client_server/tcp_socket_server.py
@@ -0,0 +1,24 @@
+import socket
+
+"""Здесь сервер принимает клиента, выводит информацию о его подключении и отправляет клиенту в ответ строку 'Hello Client!'"""
+
+host = "127.0.0.1"
+port = 12345
+
+server_sock = socket.socket()  #  объект сервера
+server_sock.bind((host, port))  #  адрес сервера, по которому он будет прослушивать подключения
+server_sock.listen(5)  #  прослушивание подключений, максимум 5 в очереди (это первый сокет на сервере)
+
+print("Socket server starts")
+
+client_sock, client_addr = server_sock.accept()  #  accept() - метод для приема входящих подключений, client_sock - второй сокет на сервере
+
+print("connection: ", client_sock)
+print("client address: ", client_addr)
+
+message = "Hello Client!"  #  сообщение для клиента
+client_sock.send(message.encode())  #  отправка сообщения
+client_sock.close()  # закрытие подключения
+
+print("Socket server stops")
+server_sock.close()
