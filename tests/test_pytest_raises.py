1717a11105acd9c62255e2cdd402e5f55375dd9a
Aleksandr S.Sakhatskii <sakhackii@protei-lab.ru>
2025-06-10 16:36:18 +0300

    использование pytest.raises

---
+++ tests/test_pytest_raises.py
@@ -0,0 +1,19 @@
+import pytest
+
+class TestPytestRaises:
+    """Способы отлавливания ошибок в тестах, чтобы тесты не падали"""
+    def test_1(self):
+        """Тест с использованием блока try/except"""
+        x = 1
+        y = 0
+        try:
+            print(x/y)
+        except ZeroDivisionError as e:
+            print(f"Ошибка: {e}")
+
+    def test_2(self):
+        """Тест с использованием pytest.raises"""
+        x = 1
+        y = 0
+        with pytest.raises(ZeroDivisionError):
+            print(x/y)
