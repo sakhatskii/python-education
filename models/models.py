c1dfebb6e5b2ceb4cb6557758019559617597519
Aleksandr S.Sakhatskii <sakhackii@protei-lab.ru>
2025-08-18 12:56:48 +0300

    added endpoint

--- models/models.py
+++ models/models.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class Numbers(BaseModel):
     num1: float
     num2: float
+
+class User(BaseModel):
+    name: str
+    id: int
--- python_education/client_server/fast_api/lesson_2_1.py
+++ python_education/client_server/fast_api/lesson_2_1.py
@@ -1,17 +1,28 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
 
-from models.models import Numbers
+from models.models import Numbers, User
 
 app = FastAPI()
 
+user = User(name="John", id=1)
 
 @app.get("/")  # curl -X GET http://127.0.0.1:4005/
 async def root():
     return {"message": "OK"}
 
 
-@app.post("/calculate",  status_code=201)
+@app.get("/custom")
+def read_custom_message():
+    return {"message": "This is a custom message!"}
+
+
+@app.get("/users", response_model=User)
+def get_user():
+    return user
+
+
+@app.post("/calculate", status_code=201)
 async def calculate(numbers: Numbers):
     """Метод Принимает два числа и возвращает их сумму.
     Команда для теста:
@@ -25,6 +36,7 @@ async def calculate(numbers: Numbers):
         return {"result": result}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
 
 if __name__ == "__main__":
     uvicorn.run("lesson_2_1:app", host="127.0.0.1", port=4005, reload=True)
